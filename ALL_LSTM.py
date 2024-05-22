adabcf1e8f0f68ac9fb9d49236efe9b4adb4d03d
Sachin Komati <sachinkomati23@gmail.com>
2024-05-21 20:45:04 -0400

    Update ALL_LSTM.py

--- ALL_LSTM.py
+++ ALL_LSTM.py
@@ -76,7 +76,7 @@ st.title('Predictive Analytics for Acute Lymphoblastic Leukemia')
 # Creating form for input
 with st.form(key='prediction_form'):
     sex = st.selectbox('Sex', options=['Male', 'Female'])
-    year = st.selectbox('Year of Mortality Rate', options=list(range(1999, 2060)))
+    year = st.selectbox('Year of Mortality Rate', options=list(range(1999, 2061)))
     agegroup = st.selectbox('Age Group', options=[
         '1-4 years', '5-9 years', '10-14 years', '15-19 years', '20-24 years', '25-29 years', '30-34 years', '35-39 years', '40-44 years',           '45-49 years', '50-54 years', '55-59 years', '60-64 years', '65-69 years', '70-74 years', '75-79 years', '80-84 years', '85+ years'
     ])
